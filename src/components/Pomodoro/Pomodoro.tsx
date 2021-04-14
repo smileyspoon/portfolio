455d1961b7f87fbd81ebe4d3e1825e9040c0a166
calvin <seng-lee@live.com>
2021-04-13 21:44:20 -0500

    got basic pomo ready

--- src/components/Pomodoro/Pomodoro.tsx
+++ src/components/Pomodoro/Pomodoro.tsx
@@ -1,17 +1,52 @@
 import React, {useState, useEffect, useCallback} from 'react';
+import { Button } from "react-bootstrap";
 // import { Row, Col, Form, Button, InputGroup, Card, Image, Carousel, Container } from "react-bootstrap";
+
+//documentation for Timer
+//https://www.npmjs.com/package/tiny-timer
 import Timer from 'tiny-timer';
 
-
+import { Time } from './Time';
 import { Loading } from '../Loading/Loading';
 
 
 export const Pomodoro = (props: any) => {
 
+
   const {user} = props;
 
 
+
+  const defaultTimer = new Timer ();
+
   const [isLoading, setIsLoading] = useState(true);
+  const [timerDisplay, setTimerDisplay] = useState(0);
+  const [newTimer, setNewTimer ] = useState(defaultTimer);
+
+
+  
+
+  //test
+  //should return {h: 2, m: 1, s: 5}
+  // const timeHMS = new Time(0,0,0,7265000);
+  // console.log(timeHMS.convertToHMS());
+
+  const start=(timer: Timer, time: Time) => {
+    timer.start(time.convertToSeconds());
+  }
+
+  const pause = (timer: Timer) => {
+    timer.pause();
+  
+  }
+  const stop = (timer: Timer) => {
+    timer.stop();
+  }
+
+  const resume = (timer: Timer) => {
+    timer.resume();
+  }
+
 
   useEffect(()=>{
 
@@ -20,11 +55,23 @@ export const Pomodoro = (props: any) => {
 
       setIsLoading(false);
 
-      const timer = new Timer()
+      
+
+      
+      //create function for each of this so it shows up on the website
+      //need to end timer on refresh
+      
+
+      // timer.on('tick', (ms) =>  console.log(Math.round(ms/1000)));
+      
 
 
-      timer.on('tick', (ms) => console.log('tick', ms))
 
+
+
+
+
+    
 
     }
 
@@ -51,7 +98,37 @@ export const Pomodoro = (props: any) => {
     (
       <div className="app">
 
+        <span>{timerDisplay}</span>
+
+        <Button onClick= {()=> {
+
+        
+          const timer = new Timer ({interval: 1000});
+          const time = new Time (0,1,3);
+          
+          timer.on('tick', (ms) =>  setTimerDisplay(Math.round(ms/1000)));
+          timer.on('done', () => console.log('done!'))
+          timer.on('statusChanged', (status) => console.log('status:', status))
+          setNewTimer(timer);
+
+
+          start(timer, time);
+
+        }}
+        
+        
+        >New Clock</Button>
+
+      <Button onClick= {()=> {stop(newTimer); }}>Stop</Button>
+
+      <Button onClick= {()=> {pause(newTimer);}}>Pause</Button>
+
+      <Button onClick= {()=> {resume(newTimer);}}>Resume</Button>
+
+
       </div>
+
+      
     )
 
   );
---
+++ src/components/Pomodoro/Time.tsx
@@ -0,0 +1,67 @@
+
+export class Time {
+
+    private ms;
+    private h;
+    private m;
+    private s;
+    
+    
+    constructor(h: number, m: number, s: number, ms=0) {
+        
+        this.h = h;
+        this.m = m;
+        this.s = s;
+        this.ms = ms;
+
+    }
+
+
+
+    getH = ()=> {
+
+        return this.h;
+    }
+
+    getM = () => {
+        return this.m;
+    }
+
+    getS = () => {
+        return this.s;
+    }
+
+    getMs =() => {
+        return this.ms;
+    }
+
+    getAll =  () => {
+
+        return {h:this.h, m: this.m, s: this.s};
+
+    }
+
+    convertToSeconds = () => {
+
+        const h = this.h * 3600000;
+        const m = this.m * 60000;
+        const s = this.s * 1000;
+    
+        return (h+m+s);
+    
+      }
+
+    convertToHMS = () =>{
+
+        const h = Math.round(this.ms/3600000);
+        const m = Math.round((this.ms%3600000)/60000);
+        const s = (this.ms - (h*3600000) - (m*60000))/1000;
+
+
+        return {h:h, m: m, s: s};
+        
+    }
+
+
+
+}
